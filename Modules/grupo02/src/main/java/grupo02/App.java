4b23a02ffdfa350ea5b03c9fc7d16ddb70756fe4
Andresf <andresr427@gmail.com>
2022-03-02 15:49:54 -0500

    Integración de las dependencias al proyecto

--- Modules/grupo02/src/main/java/grupo02/App.java
+++ Modules/grupo02/src/main/java/grupo02/App.java
@@ -4,8 +4,7 @@ package grupo02;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
